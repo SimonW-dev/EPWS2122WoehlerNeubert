f5fe2eb0a7083d44b33de2c9832ddbede4a91b5e
SimonW-dev <simon_marko.woehler@smail.th-koeln.de>
2021-11-07 14:12:21 +0100

    Animationen + CameraController

--- Main Project/Spark/Assets/PlayerController.cs
+++ Main Project/Spark/Assets/PlayerController.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Start()
+    {
+        //init
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //run left
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(-5, 0);
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
         }
+
+        //run right
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector2(5, 0);
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
         }
-        else if (Input.GetKey(KeyCode.W))
+
+        //Idle
+        else
         {
-            rb.velocity = new Vector2(0, 3);
+            anim.SetBool("running", false);
         }
+
+
+        //Jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+
     }
 }
